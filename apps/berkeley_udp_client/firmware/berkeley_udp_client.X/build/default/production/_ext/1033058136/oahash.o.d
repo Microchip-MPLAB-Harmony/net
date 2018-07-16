3eb496e4fcbb301ddc3aab585336e82d527d0940
Niyas <http://support.microchip.com>
2018-07-16 13:05:20 +0530

    removed object files

--- apps/berkeley_udp_client/firmware/berkeley_udp_client.X/build/default/production/_ext/1033058136/oahash.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/_ext/1033058136/oahash.o:  \
- ../src/config/default/library/tcpip/src/oahash.c  \
- ../src/config/default/library/tcpip/src/oahash.h 
--- apps/berkeley_udp_relay/firmware/berkeley_udp_relay.X/build/default/production/_ext/1360937237/app.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/_ext/1360937237/app.o:  \
-../src/app.c  \
-../src/app.h  \
- ../src/config/default/configuration.h  \
-../src/config/default/user.h 
