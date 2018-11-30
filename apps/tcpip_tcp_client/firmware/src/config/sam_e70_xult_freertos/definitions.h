8810ee6fb50d116281a6cb23a2b58a74ac58cc40
Niyas Hameed <niyas.hameed@microchip.com>
2018-11-30 21:45:53 +0530

    tcpip_tcp_client demo for sam_e70_xult

--- apps/tcpip_tcp_client/firmware/src/config/sam_e70_xult_freertos/definitions.h
+++ apps/tcpip_tcp_client/firmware/src/config/sam_e70_xult_freertos/definitions.h
@@ -48,20 +48,10 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/clk/plib_clk.h"
-#include "peripheral/pio/plib_pio.h"
-#include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/mpu/plib_mpu.h"
-#include "bsp/bsp.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include "system/int/sys_int.h"
-#include "osal/osal.h"
-#include "system/console/sys_console.h"
-#include "system/console/sys_debug.h"
-#include "system/console/sys_command.h"
-#include "system/time/sys_time.h"
 #include "peripheral/tc/plib_tc0.h"
+#include "system/time/sys_time.h"
+#include "bsp/bsp.h"
+#include "peripheral/usart/plib_usart1.h"
 #include "library/tcpip/tcpip.h"
 #include "driver/gmac/drv_gmac.h"
 #include "driver/miim/drv_miim.h"
@@ -70,7 +60,18 @@
 #include "system/sys_clk_h2_adapter.h"
 #include "system/sys_random_h2_adapter.h"
 #include "system/sys_reset_h2_adapter.h"
-#include "peripheral/usart/plib_usart1.h"
+#include "system/int/sys_int.h"
+#include "osal/osal.h"
+#include "peripheral/clk/plib_clk.h"
+#include "peripheral/pio/plib_pio.h"
+#include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/mpu/plib_mpu.h"
+#include "system/console/sys_console.h"
+#include "system/console/src/sys_console_uart_definitions.h"
+#include "system/console/sys_debug.h"
+#include "system/console/sys_command.h"
+#include "FreeRTOS.h"
+#include "task.h"
 #include "app.h"
 
 
@@ -193,12 +194,12 @@ void SYS_Tasks ( void );
 
 typedef struct
 {
+    SYS_MODULE_OBJ  sysTime;
+    SYS_MODULE_OBJ  tcpip;
     SYS_MODULE_OBJ  drvMiim;
     SYS_MODULE_OBJ  sysConsole0;
     SYS_MODULE_OBJ  sysDebug;
 
-    SYS_MODULE_OBJ  sysTime;
-    SYS_MODULE_OBJ  tcpip;
 
 } SYSTEM_OBJECTS;
 
--- apps/tcpip_tcp_client/firmware/src/config/sam_e70_xult_freertos/library/tcpip/src/dhcp.c
+++ apps/tcpip_tcp_client/firmware/src/config/sam_e70_xult_freertos/library/tcpip/src/dhcp.c
@@ -11,30 +11,38 @@
     - Reference: RFC 2131, 2132
 *******************************************************************************/
 
-/*******************************************************************************
-File Name:  DHCP.c
-Copyright © 2012 released Microchip Technology Inc.  All rights
-reserved.
+/*****************************************************************************
+ Copyright (C) 2012-2018 Microchip Technology Inc. and its subsidiaries.
 
-Microchip licenses to you the right to use, modify, copy and distribute
-Software only when embedded on a Microchip microcontroller or digital signal
-controller that is integrated into your product or third party product
-(pursuant to the sublicense terms in the accompanying license agreement).
+Microchip Technology Inc. and its subsidiaries.
 
-You should refer to the license agreement accompanying this Software for
-additional information regarding your rights and obligations.
+Subject to your compliance with these terms, you may use Microchip software 
+and any derivatives exclusively with Microchip products. It is your 
+responsibility to comply with third party license terms applicable to your 
+use of third party software (including open source software) that may 
+accompany Microchip software.
 
-SOFTWARE AND DOCUMENTATION ARE PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND,
-EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
-MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
-IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
-CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
-OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
-INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
-CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
-SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
-(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
-*******************************************************************************/
+THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A PARTICULAR 
+PURPOSE.
+
+IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE 
+FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN 
+ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY, 
+THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*****************************************************************************/
+
+
+
+
+
+
+
+
 #include <ctype.h>
 
 #define TCPIP_THIS_MODULE_ID    TCPIP_MODULE_DHCP_CLIENT
@@ -156,7 +164,6 @@ typedef struct
 	} validValues;
 
 #if defined TCPIP_DHCP_STORE_BOOT_FILE_NAME
-    // TODO aa: is the sname needed as well?
     uint8_t     bootFileName[TCPIP_DHCP_BOOT_FILE_NAME_SIZE];
 #endif // defined TCPIP_DHCP_STORE_BOOT_FILE_NAME
 
@@ -355,7 +362,6 @@ static const uint8_t _DHCPRequestParamsTbl[] =
 
 static __inline__ uint32_t __attribute__((always_inline)) _DHCPSecondCountGet(void)
 {
-    // TODO aa: should be replaced by a system service!
     return dhcpSecondCount;
 }
 
@@ -2096,8 +2102,6 @@ static void _DHCPSetLeaseTimeParams(DHCP_CLIENT_VARS* pClient, TCPIP_DHCP_OPTION
         false otherwise
 
   Remarks:
-    TODO aa: if adding more params for like specifying the host name,
-    a char* param may be added for DECLINE message!
  ***************************************************************************/
 
 static bool _DHCPSend(DHCP_CLIENT_VARS* pClient, TCPIP_NET_IF* pNetIf, int messageType, TCPIP_DHCP_OPERATION_FLAGS dhcpFlags)
