ffb3a34fc2d73829fba9beb33ed2798e249820f5
Adrian <http://support.microchip.com>
2018-12-04 08:38:43 -0700

    web_net_server demo addition.

---
+++ apps/web_net_server_nvm_mpfs/firmware/sam_e70_xult_freertos.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# sam_e70_xult_freertos configuration
+CND_ARTIFACT_DIR_sam_e70_xult_freertos=dist/sam_e70_xult_freertos/production
+CND_ARTIFACT_NAME_sam_e70_xult_freertos=sam_e70_xult_freertos.X.production.hex
+CND_ARTIFACT_PATH_sam_e70_xult_freertos=dist/sam_e70_xult_freertos/production/sam_e70_xult_freertos.X.production.hex
+CND_PACKAGE_DIR_sam_e70_xult_freertos=${CND_DISTDIR}/sam_e70_xult_freertos/package
+CND_PACKAGE_NAME_sam_e70_xult_freertos=same70xultfreertos.x.tar
+CND_PACKAGE_PATH_sam_e70_xult_freertos=${CND_DISTDIR}/sam_e70_xult_freertos/package/same70xultfreertos.x.tar
