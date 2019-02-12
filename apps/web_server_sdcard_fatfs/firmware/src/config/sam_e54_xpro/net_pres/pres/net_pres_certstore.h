e5e57848b00e1d70eebe3dded89ce74d6e675f0e
Hrisikesh Sahu <hrisikesh.sahu@microchip.com>
2019-02-12 17:58:57 +0530

    web_server_sdcard_fatfs

---
+++ apps/web_server_sdcard_fatfs/firmware/src/config/sam_e54_xpro/net_pres/pres/net_pres_certstore.h
@@ -0,0 +1,135 @@
+/*******************************************************************************
+MPLAB Harmony Networking Presentation Certificate Storage header file
+
+  Company:
+    Microchip Technology Inc.
+    
+  Filename:
+    net_pres_certstore.h
+    
+  Summary:
+    This file describes the standard interface for the certificate store
+    
+  Description:
+   This file describes the interface that the presentation layer uses to access
+   the certificate store.  The MHC can generate a read-only, Flash-based 
+   certificate store, or stubs for these functions.  If a more complex certificate
+   store is desired, for instance storing multiple certificates or being able to 
+   update certificates, the implementation may be provided by the end user.
+
+*******************************************************************************/
+
+//DOM-IGNORE-BEGIN
+/*****************************************************************************
+ Copyright (C) 2015-2018 Microchip Technology Inc. and its subsidiaries.
+
+Microchip Technology Inc. and its subsidiaries.
+
+Subject to your compliance with these terms, you may use Microchip software 
+and any derivatives exclusively with Microchip products. It is your 
+responsibility to comply with third party license terms applicable to your 
+use of third party software (including open source software) that may 
+accompany Microchip software.
+
+THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A PARTICULAR 
+PURPOSE.
+
+IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE 
+FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN 
+ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY, 
+THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*****************************************************************************/
+
+
+//DOM-IGNORE-END
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "configuration.h"
+
+#include "net_pres.h"
+#ifndef _NET_PRES_CERTSTORE_H_
+#define _NET_PRES_CERTSTORE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Interface Routines - System Level
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Get CA Certificates function
+
+  Summary:
+    This function gets the CA certificates from the store,
+	<p><b>Implementation:</b> Dynamic</p>
+    
+  Description:
+    This function is used by client connections to retrieve the Certificate 
+	Authority certificates that are used to validate signatures on server 
+	certificates.
+    
+  Preconditions:
+    None.
+
+  Parameters:
+    certPtr   - A pointer to the CA certificates
+    certSize  - The size of the certificates
+    certIndex - Most likely '0', but this parameter is provided to select 
+	            a different set of CA certificates
+    
+    Returns:
+    - true  - Indicates success
+    - false - Indicates failure
+	
+*/    
+
+bool NET_PRES_CertStoreGetCACerts(const uint8_t ** certPtr, int32_t * certSize, 
+                                  uint8_t certIndex);
+
+// *****************************************************************************
+/* Get Server Certificate and Key function
+
+  Summary:
+    This function gets a server certificate and key from the certificate store.
+	<p><b>Implementation:</b> Dynamic</p>
+    
+  Description:
+    This function is used by server connections to retrieve their certificate 
+	and private key. Multiple server certificates can be stored in the certificate 
+	store, for example one for a Web server and one for a mail server.
+    
+  Preconditions:
+    None.
+
+  Parameters:
+    serverCertPtr  - A pointer to the server certificate
+    serverCertSize - The size of the server certificate
+    serverKeyPtr   - A pointer to the server private key
+    serverKeySize  - The size of the server private key
+    certIndex      - Most likely '0', but this parameter is provided to select a 
+	                 different server certificate
+    
+  Returns:
+    - true  - Indicates success
+    - false - Indicates failure
+*/
+bool NET_PRES_CertStoreGetServerCert(const uint8_t ** serverCertPtr, int32_t * serverCertSize, 
+                      const uint8_t ** serverKeyPtr, int32_t * serverKeySize, uint8_t certIndex);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif //_NET_PRES_CERTSTORE_H_
--- apps/web_server_sdcard_fatfs/firmware/src/custom_http_app.c
+++ apps/web_server_sdcard_fatfs/firmware/src/custom_http_app.c
@@ -1437,8 +1437,6 @@ void TCPIP_HTTP_Print_status_fail(HTTP_CONN_HANDLE connHandle)
 
 void TCPIP_HTTP_Print_uploadedmd5(HTTP_CONN_HANDLE connHandle)
 {
-    uint8_t i;
-    uint8_t *httpDataBuff;
     TCP_SOCKET sktHTTP = TCPIP_HTTP_CurrentConnectionSocketGet(connHandle);
 
     // Set a flag to indicate not finished
@@ -1457,20 +1455,27 @@ void TCPIP_HTTP_Print_uploadedmd5(HTTP_CONN_HANDLE connHandle)
         TCPIP_HTTP_CurrentConnectionCallbackPosSet(connHandle, 0);
         return;
     }
-
-    TCPIP_TCP_StringPut(sktHTTP, (const uint8_t *)"<b>Uploaded File's MD5 was:</b><br />");
-    httpDataBuff = TCPIP_HTTP_CurrentConnectionDataBufferGet(connHandle);
-
-    // Write a byte of the md5 sum at a time
-    for(i = 0; i < 16u; ++i)
+#if defined( HTTP_APP_USE_MD5 )
+    else
     {
-        TCPIP_TCP_Put(sktHTTP, btohexa_high(httpDataBuff[i]));
-        TCPIP_TCP_Put(sktHTTP, btohexa_low(httpDataBuff[i]));
-        if((i & 0x03) == 3u)
-            TCPIP_TCP_Put(sktHTTP, ' ');
+        uint8_t     i;
+        uint8_t *   httpDataBuff;
+    
+        TCPIP_TCP_StringPut(sktHTTP, (const uint8_t *)"<b>Uploaded File's MD5 was:</b><br />");
+        httpDataBuff = TCPIP_HTTP_CurrentConnectionDataBufferGet(connHandle);
+    
+        // Write a byte of the md5 sum at a time
+        for(i = 0; i < 16u; ++i)
+        {
+            TCPIP_TCP_Put(sktHTTP, btohexa_high(httpDataBuff[i]));
+            TCPIP_TCP_Put(sktHTTP, btohexa_low(httpDataBuff[i]));
+            if((i & 0x03) == 3u)
+                TCPIP_TCP_Put(sktHTTP, ' ');
+        }
+    
+        TCPIP_HTTP_CurrentConnectionCallbackPosSet(connHandle, 0x00);
     }
-
-    TCPIP_HTTP_CurrentConnectionCallbackPosSet(connHandle, 0x00);
+#endif
 }
 
 void TCPIP_HTTP_Print_config_hostname(HTTP_CONN_HANDLE connHandle)
