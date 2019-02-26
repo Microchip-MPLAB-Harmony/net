54118b10560ad40d2134fb85063357b8573798f2
Niyas <http://support.microchip.com>
2019-02-26 21:55:39 +0530

    ipv6 exclusion for SAMA5

--- tcpip/config/tcpip_configurator_network.py
+++ tcpip/config/tcpip_configurator_network.py
@@ -30,7 +30,7 @@
 #### Code Generation ####
 ############################################################################
 def instantiateComponent(tcpipAutoConfigNetworkComponent):
-
+	processor = Variables.get("__PROCESSOR")
 	tcpipAutoConfigStackGroup = Database.findGroup("TCP/IP STACK")
 	if (tcpipAutoConfigStackGroup == None):
 		tcpipAutoConfigStackGroup = Database.createGroup(None, "TCP/IP STACK")
@@ -71,27 +71,30 @@ def instantiateComponent(tcpipAutoConfigNetworkComponent):
 	tcpipAutoConfigIPv4.setVisible(True)
 	tcpipAutoConfigIPv4.setDescription("Enable IPv4")
 	tcpipAutoConfigIPv4.setDependencies(tcpipAutoConfigIPv4Enable, ["TCPIP_AUTOCONFIG_ENABLE_IPV4"])
-	
-	# Enable IPv6
-	tcpipAutoConfigIPv6 = tcpipAutoConfigNetworkComponent.createBooleanSymbol("TCPIP_AUTOCONFIG_ENABLE_IPV6", None)
-	tcpipAutoConfigIPv6.setLabel("IPv6")
-	tcpipAutoConfigIPv6.setVisible(True)
-	tcpipAutoConfigIPv6.setDescription("Enable IPv6")
-	tcpipAutoConfigIPv6.setDependencies(tcpipAutoConfigIPv6Enable, ["TCPIP_AUTOCONFIG_ENABLE_IPV6"])
+	if "SAMA5" in processor:
+		tcpipAutoConfigIPv4.setReadOnly(True)
 
-	# Enable ICMPv6
-	tcpipAutoConfigICMPv6 = tcpipAutoConfigNetworkComponent.createBooleanSymbol("TCPIP_AUTOCONFIG_ENABLE_ICMPv6", None)
-	tcpipAutoConfigICMPv6.setLabel("ICMPv6")
-	tcpipAutoConfigICMPv6.setVisible(True)
-	tcpipAutoConfigICMPv6.setDescription("Enable ICMPv6")	
-	tcpipAutoConfigICMPv6.setDependencies(tcpipAutoConfigIcmpv6Enable, ["TCPIP_AUTOCONFIG_ENABLE_ICMPv6"])	
-	
-	# Enable NDP
-	tcpipAutoConfigNDP = tcpipAutoConfigNetworkComponent.createBooleanSymbol("TCPIP_AUTOCONFIG_ENABLE_NDP", None)
-	tcpipAutoConfigNDP.setLabel("NDP")
-	tcpipAutoConfigNDP.setVisible(True)
-	tcpipAutoConfigNDP.setDescription("Enable NDP")	
-	tcpipAutoConfigNDP.setDependencies(tcpipAutoConfigNDPEnable, ["TCPIP_AUTOCONFIG_ENABLE_NDP"])
+	if "SAMA5" not in processor:		
+		# Enable IPv6
+		tcpipAutoConfigIPv6 = tcpipAutoConfigNetworkComponent.createBooleanSymbol("TCPIP_AUTOCONFIG_ENABLE_IPV6", None)
+		tcpipAutoConfigIPv6.setLabel("IPv6")
+		tcpipAutoConfigIPv6.setVisible(True)
+		tcpipAutoConfigIPv6.setDescription("Enable IPv6")
+		tcpipAutoConfigIPv6.setDependencies(tcpipAutoConfigIPv6Enable, ["TCPIP_AUTOCONFIG_ENABLE_IPV6"])
+
+		# Enable ICMPv6
+		tcpipAutoConfigICMPv6 = tcpipAutoConfigNetworkComponent.createBooleanSymbol("TCPIP_AUTOCONFIG_ENABLE_ICMPv6", None)
+		tcpipAutoConfigICMPv6.setLabel("ICMPv6")
+		tcpipAutoConfigICMPv6.setVisible(True)
+		tcpipAutoConfigICMPv6.setDescription("Enable ICMPv6")	
+		tcpipAutoConfigICMPv6.setDependencies(tcpipAutoConfigIcmpv6Enable, ["TCPIP_AUTOCONFIG_ENABLE_ICMPv6"])	
+		
+		# Enable NDP
+		tcpipAutoConfigNDP = tcpipAutoConfigNetworkComponent.createBooleanSymbol("TCPIP_AUTOCONFIG_ENABLE_NDP", None)
+		tcpipAutoConfigNDP.setLabel("NDP")
+		tcpipAutoConfigNDP.setVisible(True)
+		tcpipAutoConfigNDP.setDescription("Enable NDP")	
+		tcpipAutoConfigNDP.setDependencies(tcpipAutoConfigNDPEnable, ["TCPIP_AUTOCONFIG_ENABLE_NDP"])
 	
 ########################################################################################################
 def finalizeComponent(tcpipAutoConfigNetworkComponent):
