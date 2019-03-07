8bf2d64ec2cb4b45df5c94e3e9e0b8fe3bc81efe
Hrisikesh Sahu <hrisikesh.sahu@microchip.com>
2019-03-07 22:47:12 +0530

    Few more demos with E70 configuration

--- apps/web_server_sdcard_fatfs/firmware/src/config/sam_e70_xult_freertos/configuration.h
+++ apps/web_server_sdcard_fatfs/firmware/src/config/sam_e70_xult_freertos/configuration.h
@@ -125,7 +125,7 @@ extern "C" {
 
 #define SYS_CMD_ENABLE
 #define SYS_CMD_DEVICE_MAX_INSTANCES       SYS_CONSOLE_DEVICE_MAX_INSTANCES
-#define SYS_CMD_PRINT_BUFFER_SIZE          1024
+#define SYS_CMD_PRINT_BUFFER_SIZE          2560
 #define SYS_CMD_BUFFER_DMA_READY
 #define SYS_CMD_REMAP_SYS_CONSOLE_MESSAGE
 /* Command System Service RTOS Configurations*/
