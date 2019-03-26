44c538f6629a0f222db9836f479486b7b3e719d9
Hrisikesh Sahu <hrisikesh.sahu@microchip.com>
2019-03-26 18:35:24 +0530

    webnet_server_sdcard demo with pic32mz_das_sk

---
+++ apps/web_net_server_sdcard_fatfs/firmware/src/config/pic32mz_das_sk/user.h
@@ -0,0 +1,50 @@
+/*******************************************************************************
+  User Configuration Header
+
+  File Name:
+    user.h
+
+  Summary:
+    Build-time configuration header for the user defined by this project.
+
+  Description:
+    An MPLAB Project may have multiple configurations.  This file defines the
+    build-time options for a single configuration.
+
+  Remarks:
+    It only provides macro definitions for build-time configuration options
+
+*******************************************************************************/
+
+#ifndef USER_H
+#define USER_H
+
+#include "bsp/bsp.h"
+#include "toolchain_specifics.h"
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: User Configuration macros
+// *****************************************************************************
+// *****************************************************************************
+#define LED_ON()                    LED1_On()
+#define BUFFER_ATTRIBUTES           __COHERENT __ALIGNED(32)
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
+
+#endif // USER_H
+/*******************************************************************************
+ End of File
+*/
