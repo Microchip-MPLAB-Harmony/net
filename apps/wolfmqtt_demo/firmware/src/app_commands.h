dc528abdadfe4e7962681fba3e2d8bb77fc60a59
Adrian Aur <adrian.aur@microchip.com>
2019-06-11 16:24:00 -0700

    [MH3-12327] - Created wolfMQTT demo

---
+++ apps/wolfmqtt_demo/firmware/src/app_commands.h
@@ -0,0 +1,59 @@
+/*******************************************************************************
+  Application Header
+
+  File Name:
+    app_commands.h
+
+  Summary:
+ code for the console commands for the simple tcp client demo
+
+  Description:
+    code for the console commands for the simple tcp client demo
+ *******************************************************************************/
+
+//DOM-IGNORE-BEGIN
+/*****************************************************************************
+ Copyright (C) 2012-2018 Microchip Technology Inc. and its subsidiaries.
+
+Microchip Technology Inc. and its subsidiaries.
+
+Subject to your compliance with these terms, you may use Microchip software 
+and any derivatives exclusively with Microchip products. It is your 
+responsibility to comply with third party license terms applicable to your 
+use of third party software (including open source software) that may 
+accompany Microchip software.
+
+THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A PARTICULAR 
+PURPOSE.
+
+IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE 
+FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN 
+ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY, 
+THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*****************************************************************************/
+//DOM-IGNORE-END
+
+#ifndef APP_COMMANDS_H
+#define	APP_COMMANDS_H
+#include <stdbool.h>
+#ifdef	__cplusplus
+extern "C" {
+#endif
+#define MAX_URL_SIZE 255
+bool APP_Commands_Init();
+extern char APP_Hostname_Buffer[MAX_URL_SIZE];
+extern char APP_Port_Buffer[6];
+extern char APP_Message_Buffer[MAX_URL_SIZE];
+extern bool APP_Send_Packet;
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* APP_COMMANDS_H */
+
