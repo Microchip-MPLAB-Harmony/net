72e34f9991604e00b2f1de71e0c28f3439b20c4c
Niyas Hameed <niyas.hameed@microchip.com>
2019-07-11 10:30:11 +0530

    tcpip_client_server - same70_freertos

--- apps/tcpip_client_server/firmware/src/config/sam_e70_xult_freertos/configuration.h
+++ apps/tcpip_client_server/firmware/src/config/sam_e70_xult_freertos/configuration.h
@@ -99,11 +99,11 @@ extern "C" {
 
 #define SYS_CMD_ENABLE
 #define SYS_CMD_DEVICE_MAX_INSTANCES       SYS_CONSOLE_DEVICE_MAX_INSTANCES
-#define SYS_CMD_PRINT_BUFFER_SIZE          1024
+#define SYS_CMD_PRINT_BUFFER_SIZE          2560
 #define SYS_CMD_BUFFER_DMA_READY
 #define SYS_CMD_REMAP_SYS_CONSOLE_MESSAGE
 /* Command System Service RTOS Configurations*/
-#define SYS_CMD_RTOS_STACK_SIZE                1024
+#define SYS_CMD_RTOS_STACK_SIZE                2560
 #define SYS_CMD_RTOS_TASK_PRIORITY             1
 
 
