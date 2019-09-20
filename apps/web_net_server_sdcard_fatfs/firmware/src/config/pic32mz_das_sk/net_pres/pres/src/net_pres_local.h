7dcd5053987bbd5ffdac68e72ac4400c69f78521
Adrian <http://support.microchip.com>
2019-09-20 13:22:10 -0700

    [MH3-22283] - Updated net_pres.c to free the provide context only when the context is open.

--- apps/web_net_server_sdcard_fatfs/firmware/src/config/pic32mz_das_sk/net_pres/pres/src/net_pres_local.h
+++ apps/web_net_server_sdcard_fatfs/firmware/src/config/pic32mz_das_sk/net_pres/pres/src/net_pres_local.h
@@ -1,3 +1,26 @@
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
 #ifndef _NET_PRES_LOCAL_H_
 #define _NET_PRES_LOCAL_H_
 
@@ -27,14 +50,15 @@ extern "C" {
     
     typedef struct _NET_PRES_SocketData
     {
-        bool inUse;
-        NET_PRES_SKT_ERROR_T lastError;
-        NET_PRES_SKT_T socketType;
-        int16_t transHandle;
-        uint8_t providerData[8];
-        NET_PRES_EncSessionStatus status;
+        uint8_t     inUse;
+        uint8_t     provOpen;
+        uint8_t     status;     // NET_PRES_EncSessionStatus value
+        int8_t      lastError;  // NET_PRES_SKT_ERROR_T value
+        uint16_t    socketType; // NET_PRES_SKT_T value
+        int16_t     transHandle;
         NET_PRES_TransportObject * transObject;
         NET_PRES_EncProviderObject * provObject;
+        uint8_t providerData[8];
     }NET_PRES_SocketData;
     
 #ifdef __CPLUSPLUS
