91fb184ffc84c9c07b7e4afbe6a8bedabd01992e
Adrian Aur <adrian.aur@microchip.com>
2019-10-04 11:31:25 -0700

    [MH3-22733] - Added external registration for external IPv4 processing.

--- tcpip/ipv4.h
+++ tcpip/ipv4.h
@@ -386,6 +386,78 @@ typedef struct
 }TCPIP_IPV4_PACKET_PARAMS;
 
 // *****************************************************************************
+/*
+  Type:
+    TCPIP_IPV4_PROCESS_HANDLE
+
+  Summary:
+    Defines a IPv4 packet processing handle.
+
+  Description:
+    Definition of an packet processing handle used for
+    packet processing registration by the IPv4 clients.
+
+*/
+typedef const void* TCPIP_IPV4_PROCESS_HANDLE;
+
+// *****************************************************************************
+/* IPv4 packet handler Pointer
+
+  Function:
+    bool <FunctionName> (TCPIP_NET_HANDLE hNet, struct _tag_TCPIP_MAC_PACKET* rxPkt, const void* hParam);
+
+  Summary:
+    Pointer to a function(handler) that will get called to process an incoming IPv4 packet.
+
+  Description:
+    Pointer to a function that will be called by the IPv4 module
+    when a RX packet is available.
+
+  Precondition:
+    None
+
+  Parameters:
+    hNet        - network handle on which the packet has arrived
+    rxPkt       - pointer to incoming packet
+    hParam      - user passed parameter when handler was registered
+
+  Returns:
+    true - if the packet is processed by the external handler.
+           In this case the IPv4 module will no longer process the packet
+    false - the packet needs to be processed internally by the IPv4 as usual           
+
+  Remarks:
+    The packet handler is called in the IPv4 context.
+    The handler should be kept as short as possible as it affects the processing of all the other
+    IPv4 RX traffic.
+
+    The rxPkt->pNetLayer points to an IPV4_HEADER data structure.
+    Before calling the external packet handler minimal checks are done on the packet
+    (and if these checks fail, the packet will be immediately discarded):
+    - correct header version
+    - correct header length
+    - correct packet source address 
+    Also:
+    - the rxPkt->pktIf is updated
+    - checksum is NOT verified!
+
+    Important!
+    When the packet handler returns true, once it's done processing the packet,
+    it needs to acknowledge it, i.e. return to the owner,
+    which is the MAC driver serving the network interface!
+    This means that the packet acknowledge function needs to be called,
+    with a proper acknowledge parameter and the QUEUED flag needs to be cleared, if needed:
+    if((*rxPkt->ackFunc)(rxPkt, rxPkt->ackParam))
+    {
+           rxPkt->pktFlags &= ~TCPIP_MAC_PKT_FLAG_QUEUED;
+    }
+    Failure to do that will result in memory leaks and starvation of the MAC driver.
+    See the tcpip_mac.h for details.
+    
+ */
+typedef bool(*TCPIP_IPV4_PACKET_HANDLER)(TCPIP_NET_HANDLE hNet, struct _tag_TCPIP_MAC_PACKET* rxPkt, const void* hParam);
+
+// *****************************************************************************
 // *****************************************************************************
 // Section: Public Functions
 // *****************************************************************************
@@ -884,6 +956,82 @@ int TCPIP_IPV4_MaxDatagramDataSizeGet(TCPIP_NET_HANDLE netH);
 
  */
 bool TCPIP_IPV4_IsFragmentationEnabled(void);
+
+//*******************************************************************************
+/*
+  Function:
+    TCPIP_IPV4_PROCESS_HANDLE    TCPIP_IPV4_PacketHandlerRegister(TCPIP_IPV4_PACKET_HANDLER pktHandler, const void* handlerParam)
+
+  Summary:
+    Sets a new packet processing handler.
+
+  Description:
+    This function registers a new packet processing handler.
+    The caller can use the handler to be notified of incoming packets
+    and given a chance to examine/process them.
+
+  Precondition:
+    IPv4 properly initialized
+
+  Parameters:
+    pktHandler      - the packet handler which will be called for an incoming packet
+    handlerParam    - packet handler parameter
+
+  Returns:
+    - a valid TCPIP_IPV4_PROCESS_HANDLE - if the operation succeeded
+    - NULL - if the operation failed
+
+  Example:
+    <code>
+    TCPIP_IPV4_PROCESS_HANDLE pktHandle = TCPIP_IPV4_PacketHandlerRegister( myPktHandler, myParam );
+    </code>
+
+  Remarks:
+    Currently only one packet handler is supported for the IPv4 module.
+    The call will fail if a handler is already registered.
+    Use TCPIP_IPV4_PacketHandlerDeregister first
+
+    Exists only if TCPIP_IPV4_EXTERN_PACKET_PROCESS is true 
+
+  */
+TCPIP_IPV4_PROCESS_HANDLE     TCPIP_IPV4_PacketHandlerRegister(TCPIP_IPV4_PACKET_HANDLER pktHandler, const void* handlerParam);
+
+
+//*******************************************************************************
+/*
+  Function:
+    bool    TCPIP_IPV4_PacketHandlerDeregister(TCPIP_IPV4_PROCESS_HANDLE pktHandle);
+
+  Summary:
+    Deregisters a previously registered packet handler.
+
+  Description:
+    This function removes a packet processing handler.
+
+  Precondition:
+    IPv4 properly initialized
+
+  Parameters:
+    pktHandle   - TCPIP packet handle obtained by a call to TCPIP_IPV4_PacketHandlerRegister
+
+
+  Returns:
+    - true  - if the operation succeeded
+    - false - if the operation failed
+
+  Example:
+    <code>
+    TCPIP_IPV4_PROCESS_HANDLE myHandle = TCPIP_IPV4_PacketHandlerRegister(myPacketHandler, myParam );
+    // process incoming packets
+    // now we're done with it
+    TCPIP_IPV4_PacketHandlerDeregister(myHandle);
+    </code>
+
+  Remarks:
+    Exists only if TCPIP_IPV4_EXTERN_PACKET_PROCESS is true 
+
+  */
+bool    TCPIP_IPV4_PacketHandlerDeregister(TCPIP_IPV4_PROCESS_HANDLE pktHandle);
 
 
 // *****************************************************************************
