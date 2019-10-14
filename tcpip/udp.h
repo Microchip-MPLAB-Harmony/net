3cf13d7d3095c71f786435ab973776350c289f35
Adrian Aur <adrian.aur@microchip.com>
2019-10-14 07:57:08 -0700

    [MH3-23145] - Added clarifications for UDP_OPTION_MULTICAST/UDP_OPTION_MULTICAST_DATA option.

--- tcpip/udp.h
+++ tcpip/udp.h
@@ -298,6 +298,35 @@ typedef enum
 
   Description:
     Allows the multicast options configuration of a socket.
+
+  Remarks:
+    Using the mask and value members, multiple flags can be set and cleared in one single operations.
+    The TCPIP_UDP_OptionsSet() operation for UDP_OPTION_MULTICAST will apply the folowing to the socket flags:
+    flags &= ~flagsMask;    // clear all flags touched by mask
+    flags |= (flagsValue & flagsMask);  // set the corresponding flags that are set in flagsValue. All others will remain cleared.
+    
+
+  Example:
+    <code>
+    // set the flag UDP_MCAST_FLAG_LOOP:
+    UDP_OPTION_MULTICAST_DATA mcastData;
+    mcastData.flagsMask = UDP_MCAST_FLAG_LOOP;      // specify the flag to touch
+    mcastData.flagsValue = UDP_MCAST_FLAG_LOOP;     // new value for the flag:
+                                                    // any value with the bit set for the UDP_MCAST_FLAG_LOOP bit
+    TCPIP_UDP_OptionsSet(hUdp, UDP_OPTION_MULTICAST, &mcastData);
+
+    // clear the flag UDP_MCAST_FLAG_LOOP:
+    mcastData.flagsMask = UDP_MCAST_FLAG_LOOP;      // specify the flag to touch
+    mcastData.flagsValue = 0;                       // new value for the flag:
+                                                    // any value with the bit cleared for the UDP_MCAST_FLAG_LOOP bit
+    TCPIP_UDP_OptionsSet(hUdp, UDP_OPTION_MULTICAST, &mcastData);
+
+    // set UDP_MCAST_FLAG_LOOP and UDP_MCAST_FLAG_DISABLE_SOURCE_CHECK
+    // clear  UDP_MCAST_FLAG_IGNORE_SOURCE_ADD and UDP_MCAST_FLAG_IGNORE_SOURCE_PORT
+    mcastData.flagsMask = UDP_MCAST_FLAG_LOOP | UDP_MCAST_FLAG_DISABLE_SOURCE_CHECK | UDP_MCAST_FLAG_IGNORE_SOURCE_ADD | UDP_MCAST_FLAG_IGNORE_SOURCE_PORT; // flags to touch
+    mcastData.flagsValue = UDP_MCAST_FLAG_LOOP | UDP_MCAST_FLAG_DISABLE_SOURCE_CHECK ;  // value with bits set for the first 2 flags, cleared for the others
+    TCPIP_UDP_OptionsSet(hUdp, UDP_OPTION_MULTICAST, &mcastData);
+    </code>
 */
 //
 typedef struct
