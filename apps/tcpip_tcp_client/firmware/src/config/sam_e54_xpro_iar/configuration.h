876e2a9217f47837a7bb364be61358ddbfce4c56
Niyas <http://support.microchip.com>
2019-11-08 20:18:28 +0530

    MH3-23553 MH3-23551 MH3-23549 MH3-23545 MH3-23543 MH3-23541

--- apps/tcpip_tcp_client/firmware/src/config/sam_e54_xpro_iar/configuration.h
+++ apps/tcpip_tcp_client/firmware/src/config/sam_e54_xpro_iar/configuration.h
@@ -138,7 +138,6 @@ extern "C" {
 
 
 
-
 // *****************************************************************************
 // *****************************************************************************
 // Section: Middleware & Other Library Configuration
@@ -330,8 +329,6 @@ extern "C" {
 
 
 
-
-/*** TCP/IP Configuration ***/
 
 
 /*** TCPIP MAC Configuration ***/
