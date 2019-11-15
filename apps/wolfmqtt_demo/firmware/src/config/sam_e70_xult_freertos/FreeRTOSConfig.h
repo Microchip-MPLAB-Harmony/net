95ded563b0b3c017c0f20719c3c9efaa3b992be9
Adrian <http://support.microchip.com>
2019-11-15 13:05:37 -0700

    [MH3-24195], [MH3-24197] - Test

--- apps/wolfmqtt_demo/firmware/src/config/sam_e70_xult_freertos/FreeRTOSConfig.h
+++ apps/wolfmqtt_demo/firmware/src/config/sam_e70_xult_freertos/FreeRTOSConfig.h
@@ -50,7 +50,7 @@
 #define configMINIMAL_STACK_SIZE                ( 128 )
 #define configSUPPORT_DYNAMIC_ALLOCATION        1
 #define configSUPPORT_STATIC_ALLOCATION         0
-#define configTOTAL_HEAP_SIZE                   ( ( size_t ) 40960 )
+#define configTOTAL_HEAP_SIZE                   ( ( size_t ) 80960 )
 #define configMAX_TASK_NAME_LEN                 ( 16 )
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
