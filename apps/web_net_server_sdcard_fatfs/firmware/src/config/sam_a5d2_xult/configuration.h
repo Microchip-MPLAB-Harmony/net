1359bd4d62e34a0753da318e5a9dbb012f9448e4
Adrian <http://support.microchip.com>
2019-11-19 15:04:25 -0700

    [MH3-24042], [MH3-24044] - Test

--- apps/web_net_server_sdcard_fatfs/firmware/src/config/sam_a5d2_xult/configuration.h
+++ apps/web_net_server_sdcard_fatfs/firmware/src/config/sam_a5d2_xult/configuration.h
@@ -217,7 +217,7 @@ extern "C" {
 #define TCPIP_HTTP_NET_MAX_CONNECTIONS		    		4
 #define TCPIP_HTTP_NET_DEFAULT_FILE		        		"index.htm"
 #define TCPIP_HTTP_NET_FILENAME_MAX_LEN			        25
-#define TCPIP_HTTP_NET_WEB_DIR		        		    "/mnt/mchpSite1/"
+#define TCPIP_HTTP_NET_WEB_DIR		        		    "/mnt/mchpSite1/web_pages"
 #define TCPIP_HTTP_NET_USE_POST
 #define TCPIP_HTTP_NET_USE_COOKIES
 #define TCPIP_HTTP_NET_USE_BASE64_DECODE
