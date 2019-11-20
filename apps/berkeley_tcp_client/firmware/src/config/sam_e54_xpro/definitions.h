b5282f11ef9ebdb15be77f26a0406f9c634fe767
Niyas <http://support.microchip.com>
2019-11-20 16:49:49 +0530

    MH3-24420

--- apps/berkeley_tcp_client/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/berkeley_tcp_client/firmware/src/config/sam_e54_xpro/definitions.h
@@ -48,6 +48,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "system/time/sys_time.h"
 #include "net_pres/pres/net_pres.h"
