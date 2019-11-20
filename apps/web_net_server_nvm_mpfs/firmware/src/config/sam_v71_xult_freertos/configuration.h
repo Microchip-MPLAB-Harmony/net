d8a3f4f52adff28e68f58cfe4ddfc58425d49f65
Hrisikesh <http://support.microchip.com>
2019-11-20 20:01:22 +0530

    web_net_server_nvm_mpfs MH3-24012,MH3-24014 tested other demos, found some correction w.r.t command and performance.

--- apps/web_net_server_nvm_mpfs/firmware/src/config/sam_v71_xult_freertos/configuration.h
+++ apps/web_net_server_nvm_mpfs/firmware/src/config/sam_v71_xult_freertos/configuration.h
@@ -121,7 +121,7 @@ extern "C" {
 
 #define SYS_CMD_ENABLE
 #define SYS_CMD_DEVICE_MAX_INSTANCES       SYS_CONSOLE_DEVICE_MAX_INSTANCES
-#define SYS_CMD_PRINT_BUFFER_SIZE          1024
+#define SYS_CMD_PRINT_BUFFER_SIZE          2560
 #define SYS_CMD_BUFFER_DMA_READY
 #define SYS_CMD_REMAP_SYS_CONSOLE_MESSAGE
 /* Command System Service RTOS Configurations*/
@@ -505,7 +505,7 @@ extern "C" {
 /*** TCPIP Heap Configuration ***/
 
 #define TCPIP_STACK_USE_INTERNAL_HEAP
-#define TCPIP_STACK_DRAM_SIZE                       64960
+#define TCPIP_STACK_DRAM_SIZE                       69960
 #define TCPIP_STACK_DRAM_RUN_LIMIT                  2048
 
 #define TCPIP_STACK_MALLOC_FUNC                     malloc
