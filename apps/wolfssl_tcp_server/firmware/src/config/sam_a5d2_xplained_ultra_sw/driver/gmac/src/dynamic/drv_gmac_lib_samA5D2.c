a0042ad043b646859c069e91e015b00c284abb57
Adrian Aur <Adrian.Aur@microchip.com>
2020-04-02 14:17:58 -0700

    [MH3-30992], [MH3-30993] - wolfssl_tcp_server - SAMA5D2_sw Regeneration and Test

--- apps/wolfssl_tcp_server/firmware/src/config/sam_a5d2_xplained_ultra_sw/driver/gmac/src/dynamic/drv_gmac_lib_samA5D2.c
+++ apps/wolfssl_tcp_server/firmware/src/config/sam_a5d2_xplained_ultra_sw/driver/gmac/src/dynamic/drv_gmac_lib_samA5D2.c
@@ -113,6 +113,10 @@ void DRV_PIC32CGMAC_LibInit(DRV_GMAC_DRIVER* pMACDrv)
     // pause enable, remove FCS, MDC clock
     GMAC_REGS->GMAC_NCFGR =  GMAC_NCFGR_FD_Msk | (GMAC_NCFGR_DBW_Msk & ((0) << GMAC_NCFGR_DBW_Pos))
                           | GMAC_NCFGR_CLK_MCK_64 | GMAC_NCFGR_PEN_Msk | GMAC_NCFGR_RFCS_Msk;    
+	if(DRV_GMAC_RX_CHKSM_OFFLOAD != TCPIP_MAC_CHECKSUM_NONE)
+    {
+        GMAC_REGS->GMAC_NCFGR |= GMAC_NCFGR_RXCOEN_Msk;
+    }
     // Set MAC address
     DRV_PIC32CGMAC_LibSetMacAddr((const uint8_t *)(pMACDrv->sGmacData.gmacConfig.macAddress.v));
 	if((TCPIP_INTMAC_PHY_CONFIG_FLAGS) & DRV_ETHPHY_CFG_RMII)
@@ -159,7 +163,11 @@ DRV_PIC32CGMAC_RESULT DRV_PIC32CGMAC_LibInitTransfer(DRV_GMAC_DRIVER* pMACDrv,GM
         GMAC_REGS->GMAC_DCFGR = (GMAC_DCFGR_DRBS_Msk & ((wRxBufferSize_temp >> 6) << GMAC_DCFGR_DRBS_Pos))
                 | (GMAC_DCFGR_RXBMS_Msk & ((3) << GMAC_DCFGR_RXBMS_Pos))
                 | GMAC_DCFGR_TXPBMS_Msk | GMAC_DCFGR_FBLDO_INCR4 | GMAC_DCFGR_DDRP_Msk;
-
+	
+		if(DRV_GMAC_TX_CHKSM_OFFLOAD != TCPIP_MAC_CHECKSUM_NONE)
+        {
+            GMAC_REGS->GMAC_DCFGR |= GMAC_DCFGR_TXCOEN_Msk;
+        }
         // enable GMAC interrupts
         GMAC_REGS->GMAC_IER = GMAC_INT_BITS;
     }
