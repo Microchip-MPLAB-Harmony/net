7075a14df8db6c27643e09af8a98061fa6405432
Adrian Aur <Adrian.Aur@microchip.com>
2020-06-16 10:10:22 -0700

    [MH3-30897] - Test of the web_net_server_qspi_mpfs FreeRTOS demo

--- apps/web_net_server_qspi_mpfs/firmware/src/config/sam_a5d2_xult_freertos/definitions.h
+++ apps/web_net_server_qspi_mpfs/firmware/src/config/sam_a5d2_xult_freertos/definitions.h
@@ -54,6 +54,7 @@
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
+#include "system/debug/sys_debug.h"
 #include "driver/mx25l/drv_mx25l.h"
 #include "system/fs/sys_fs.h"
 #include "system/fs/sys_fs_media_manager.h"
