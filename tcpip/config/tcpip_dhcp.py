c27e79844d87d3e7aeabe3f2cc907fcbd93403c0
Adrian Aur <Adrian.Aur@microchip.com>
2020-08-04 13:35:39 -0700

    [MH3-38719] - Updated the default DHCP client timeout to 10 seconds

--- tcpip/config/tcpip_dhcp.py
+++ tcpip/config/tcpip_dhcp.py
@@ -39,7 +39,7 @@ def instantiateComponent(tcpipDhcpComponent):
     tcpipDhcpcReqTimeout.setLabel("DHCP Request Time-out (seconds)")
     tcpipDhcpcReqTimeout.setVisible(True)
     tcpipDhcpcReqTimeout.setDescription("DHCP Request Time-out in seconds")
-    tcpipDhcpcReqTimeout.setDefaultValue(2)
+    tcpipDhcpcReqTimeout.setDefaultValue(10)
     #tcpipDhcpcReqTimeout.setDependencies(tcpipDhcpMenuVisibleSingle, ["TCPIP_STACK_USE_DHCP_CLIENT"])
 
     # DHCP Client Tick Rate in msec
