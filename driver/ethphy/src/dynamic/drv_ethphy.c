03e7215e671f090a13400ee2ba7721921166fe2c
Adrian Aur <Adrian.Aur@microchip.com>
2020-08-31 12:36:12 -0700

    [MH3-44156] - Removed PHY hardware configuration dependency of xc32 version

--- driver/ethphy/src/dynamic/drv_ethphy.c
+++ driver/ethphy/src/dynamic/drv_ethphy.c
@@ -1510,18 +1510,17 @@ DRV_ETHPHY_RESULT DRV_ETHPHY_HWConfigFlagsGet( DRV_HANDLE handle, DRV_ETHPHY_CON
     }
 
     // get the way the hw is configured
-#if (__XC_VERSION <= 2400) && defined(__PIC32MZ) && (__PIC32_FEATURE_SET0=='W')
-    // PIC32MZW1
+#if defined(TCPIP_IF_PIC32MZW1)
     hwFlags = DRV_ETHPHY_CFG_RMII;
     ethRes = DRV_ETHPHY_RES_OK;
-#elif defined (__PIC32MX__) || defined(__PIC32MZ__)
+#elif defined (TCPIP_IF_ETHMAC)
     hwFlags =  (DEVCFG3bits.FMIIEN != 0) ?     DRV_ETHPHY_CFG_MII : DRV_ETHPHY_CFG_RMII;
     hwFlags |= (DEVCFG3bits.FETHIO != 0) ? DRV_ETHPHY_CFG_DEFAULT : DRV_ETHPHY_CFG_ALTERNATE;
     ethRes = DRV_ETHPHY_RES_OK;
-#elif defined (__PIC32C__) || defined(__SAMA5D2__)
+#elif defined (TCPIP_IF_GMAC)
     hwFlags = ((GMAC_REGS->GMAC_UR & GMAC_UR_Msk)== DRV_GMAC_RMII_MODE) ?  DRV_ETHPHY_CFG_RMII : DRV_ETHPHY_CFG_MII;
     ethRes = DRV_ETHPHY_RES_OK;
-#elif defined (__SAM9X60__)
+#elif defined(TCPIP_IF_EMAC0) || defined(TCPIP_IF_EMAC1)
     hwFlags = DRV_ETHPHY_CFG_RMII;
     ethRes = DRV_ETHPHY_RES_OK;
 #else
