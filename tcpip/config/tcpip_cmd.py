afed5224997aa0d91a0b4234cc57aa5236e00f7b
Hrisikesh Sahu <hrisikesh.sahu@microchip.com>
2021-12-13 16:54:54 +0530

    [MH3-61243] there was a tcpip command python file indent issue  and due to that it was not getting added to the MHC project

--- tcpip/config/tcpip_cmd.py
+++ tcpip/config/tcpip_cmd.py
@@ -27,7 +27,7 @@ def instantiateComponent(tcpipCmdComponent):
 		
 	# Use TCP/IP Commands
 	tcpipCmd = tcpipCmdComponent.createBooleanSymbol("TCPIP_STACK_USE_COMMANDS", None)
-    tcpipCmd.setHelp("mcc_h3_tcpipcmd_configurations")
+	tcpipCmd.setHelp("mcc_h3_tcpipcmd_configurations")
 	tcpipCmd.setLabel("Use TCP/IP Commands")
 	tcpipCmd.setVisible(False)
 	tcpipCmd.setDescription("Use TCP/IP Commands")
@@ -35,7 +35,7 @@ def instantiateComponent(tcpipCmdComponent):
 
 	# Enable Storage for Stack Commands
 	tcpipCmdStorage = tcpipCmdComponent.createBooleanSymbol("TCPIP_STACK_COMMANDS_STORAGE_ENABLE", None)
-    tcpipCmdStorage.setHelp("mcc_h3_tcpipcmd_configurations")
+	tcpipCmdStorage.setHelp("mcc_h3_tcpipcmd_configurations")
 	tcpipCmdStorage.setLabel("Enable Storage for Stack Commands")
 	tcpipCmdStorage.setVisible((Database.getSymbolValue("tcpipStack", "TCPIP_STACK_CONFIGURATION_SAVE_RESTORE") == True))
 	tcpipCmdStorage.setDescription("Enable Storage for Stack Commands")
@@ -69,23 +69,23 @@ def tcpipCmdStorageMenuVisible(symbol, event):
 
 #Set symbols of other components
 def setVal(component, symbol, value):
-    triggerDict = {"Component":component,"Id":symbol, "Value":value}
-    if(Database.sendMessage(component, "SET_SYMBOL", triggerDict) == None):
-        print "Set Symbol Failure" + component + ":" + symbol + ":" + str(value)
-        return False
-    else:
-        return True
+	triggerDict = {"Component":component,"Id":symbol, "Value":value}
+	if(Database.sendMessage(component, "SET_SYMBOL", triggerDict) == None):
+		print "Set Symbol Failure" + component + ":" + symbol + ":" + str(value)
+		return False
+	else:
+		return True
 
 #Handle messages from other components
 def handleMessage(messageID, args):
-    retDict= {}
-    if (messageID == "SET_SYMBOL"):
-        print "handleMessage: Set Symbol"
-        retDict= {"Return": "Success"}
-        Database.setSymbolValue(args["Component"], args["Id"], args["Value"])
-    else:
-        retDict= {"Return": "UnImplemented Command"}
-    return retDict
-    
+	retDict= {}
+	if (messageID == "SET_SYMBOL"):
+		print "handleMessage: Set Symbol"
+		retDict= {"Return": "Success"}
+		Database.setSymbolValue(args["Component"], args["Id"], args["Value"])
+	else:
+		retDict= {"Return": "UnImplemented Command"}
+	return retDict
+	
 def destroyComponent(component):
 	Database.setSymbolValue("tcpipCmd", "TCPIP_STACK_USE_COMMANDS", False, 2)
