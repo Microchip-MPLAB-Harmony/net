220957f138a0bc01619d22657c3df2155c0e5d5e
Adrian Aur <Adrian.Aur@microchip.com>
2023-03-06 14:42:28 -0700

    [MH3-74727] - Updated the disclaimer for the java files

--- utilities/tcpip_discoverer/src/src/TCPIP_Discoverer/TcpIpDiscoverTool.java
+++ utilities/tcpip_discoverer/src/src/TCPIP_Discoverer/TcpIpDiscoverTool.java
@@ -1,4 +1,3 @@
-
 /*********************************************************************
 *
 *  TCPIPDiscoverer UI with a UDP socket( 30303) to transmit IPV4 broadcast
@@ -8,30 +7,31 @@
 * FileName:        TcpIpDiscoverTool.java
 * Complier:        JAVA version "1.6.0_20 and onwards"
 * IDE :            Netbean
-* Software License Agreement
-*
-* Copyright (C) 2012 Microchip Technology Inc.  All rights reserved.
-*
-* Microchip licenses to you the right to use, modify, copy, and
-* distribute the Software when used with a Microchip microcontroller or
-* digital signal controller product which is integrated into Licensee's product.
-*
-* You should refer to the license agreement accompanying this
-* Software for additional information regarding your rights and
-* obligations.
-*
-* THE SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT
-* WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
-* LIMITATION, ANY WARRANTY OF MERCHANTABILITY, FITNESS FOR A
-* PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT SHALL
-* MICROCHIP BE LIABLE FOR ANY INCIDENTAL, SPECIAL, INDIRECT OR
-* CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF
-* PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR SERVICES, ANY CLAIMS
-* BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE
-* THEREOF), ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER
-* SIMILAR COSTS, WHETHER ASSERTED ON THE BASIS OF CONTRACT, TORT
-* (INCLUDING NEGLIGENCE), BREACH OF WARRANTY, OR OTHERWISE
-**********************************************************************/
+*/
+
+/*
+Copyright (C) 2012-2023, Microchip Technology Inc., and its subsidiaries. All rights reserved.
+
+The software and documentation is provided by microchip and its contributors
+"as is" and any express, implied or statutory warranties, including, but not
+limited to, the implied warranties of merchantability, fitness for a particular
+purpose and non-infringement of third party intellectual property rights are
+disclaimed to the fullest extent permitted by law. In no event shall microchip
+or its contributors be liable for any direct, indirect, incidental, special,
+exemplary, or consequential damages (including, but not limited to, procurement
+of substitute goods or services; loss of use, data, or profits; or business
+interruption) however caused and on any theory of liability, whether in contract,
+strict liability, or tort (including negligence or otherwise) arising in any way
+out of the use of the software and documentation, even if advised of the
+possibility of such damage.
+
+Except as expressly permitted hereunder and subject to the applicable license terms
+for any third-party software incorporated in the software and any applicable open
+source software license terms, no license or other rights, whether express or
+implied, are granted under any patent or other intellectual property rights of
+Microchip or any third party.
+*/
+
 package TCPIP_Discoverer;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.*;
