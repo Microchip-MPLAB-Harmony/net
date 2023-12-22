d936af1bcb35c390f2dbed199d923b854e712cb2
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-12-22 18:50:33 +0530

    [MH3-89233] - Updated the IPERF throughput results in the net documentation

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20231219043635';
+                args = '20231222124840';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
