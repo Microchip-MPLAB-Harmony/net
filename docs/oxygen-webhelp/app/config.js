ccc4f8d34bed545db1b57f427dc36d9a47d2b546
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2024-08-13 19:59:18 +0530

    [MH3-97851] - Updated the Network Metrics page in net repo documentation

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20240715033840';
+                args = '20240813134954';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
